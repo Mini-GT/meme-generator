8b8711582cba0c27d71b8178a3875e8c2aa2c1b8
MiniGT <bendanillo539@gmail.com>
2024-10-05 19:37:51 +0800

    tracked last name

---
+++ src/challenges/formInput/part 2/FormInput.tsx
@@ -0,0 +1,49 @@
+import { useState } from "react"
+
+export default function FormInput() {
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: ""
+  })
+  /**
+   * Challenge: Track the applicant's last name as well
+   */
+  
+  function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+    const {name, value} = e.target;
+
+    setFormData(prevFormData => {
+      return {
+        ...prevFormData,
+        [name]: value
+      }
+    })
+
+  }
+
+  
+  return (
+    
+    <form>
+      <p>{formData.firstName}</p>
+      <input
+        type="text"
+        placeholder="First Name"
+        onChange={handleChange}
+        name="firstName"
+        // always add value attribute to the form input to ensure that it is in sync with the state
+        value={formData.firstName}
+      />
+
+      <p>{formData.lastName}</p>
+      <input
+        type="text"
+        placeholder="Last Name"
+        onChange={handleChange}
+        name="lastName"
+        value={formData.lastName}
+      />
+    </form>
+    
+  )
+}
