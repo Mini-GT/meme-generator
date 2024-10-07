29237697e8396493a4b2ed41c9e56b804b8e0ec0
MiniGT <bendanillo539@gmail.com>
2024-10-07 23:50:35 +0800

    learned submit button inside a form and how to handle the submit btn

--- src/challenges/formInput/select and input/Select.tsx
+++ src/challenges/formInput/select and input/Select.tsx
@@ -6,12 +6,12 @@ export default function Select() {
       lastName: "", 
       email: "", 
       comments: "", 
-      isFriendly: true,
+      isFriendly: false,
       employment: "",
       favColor: ""
     }
   )
-  console.log(formData.favColor)
+  // console.log(formData.favColor)
   
   function handleChange(e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement |HTMLSelectElement>) {
     const {name, value, type} = e.target
@@ -23,9 +23,16 @@ export default function Select() {
       }
     })
   }
+
+  function handleSubmit(e: React.ChangeEvent<HTMLFormElement>) {
+    //preventDefault() will prevent the page to refresh when the submit button is clicked / in other words it will prevent our app to re-render, after that, it will put our obj properties into the url params
+    e.preventDefault()
+    console.log(formData)
+  }
   
   return (
-    <form>
+    // when a button is found inside the form, it will trigger the form handler onSubmit={}
+    <form onSubmit={handleSubmit}>
       <input
         type="text"
         placeholder="First Name"
@@ -104,6 +111,7 @@ export default function Select() {
       <label htmlFor="favColor">What is your favorite color?</label>
       <br />
       <select 
+        className="border"
         id="favColor"
         name="favColor"
         value={formData.favColor}
@@ -118,6 +126,10 @@ export default function Select() {
         <option value="indigo">Indigo</option>
         <option value="violet">Violet</option>
       </select>
+      <br />
+      <br />
+      {/* button element inside a form will act as a submit, therefore it has a default attribute of type="submit" */}
+      <button className="border">Submit</button>
     </form>
   )
 }
