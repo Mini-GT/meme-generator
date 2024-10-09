0a8a924bc00c37d67baaaf994cb304f267395f29
MiniGT <bendanillo539@gmail.com>
2024-10-09 22:33:28 +0800

    changed to the correct reference

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-// import App from './App.tsx'
+import App from './App.tsx'
 // import App from './challenges/outputRandomURL/App'
 // import App from './challenges/addThingToTheArray/App'
 // import App from './challenges/statePractice/App'
@@ -13,7 +13,7 @@ import { createRoot } from 'react-dom/client'
 // import FormInput from './challenges/formInput/part 3/FormInput'
 // import Radio from './challenges/formInput/radio/Radio'
 // import Select from './challenges/formInput/select and input/Select'
-import App from './challenges/formInput/final challenge/App'
+// import App from './challenges/formInput/final challenge/App'
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(
