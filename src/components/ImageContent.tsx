fa1dcb24018ac555b19d079a65eb5ad019ca7f0d
MiniGT <bendanillo539@gmail.com>
2024-10-09 22:35:51 +0800

    updated ImageContent props

--- src/components/ImageContent.tsx
+++ src/components/ImageContent.tsx
@@ -1,15 +1,21 @@
-// import { useState } from "react"
+import { MemeTypes } from "./types"
 
 type ImageContentProps = {
   imgUrl: string
+  memeData: MemeTypes
 }
 
-export default function ImageContent({imgUrl}: ImageContentProps) {
+export default function ImageContent({
+  imgUrl,
+  memeData
+}: ImageContentProps) {
   // const [memeImg, setMemeImg] = useState("/imgs/meme.png")
 
   return (
     <div className="m-4 flex justify-center items-center">
       <img className="w-3/4" src={imgUrl} alt="meme" />
+      <h2 className="meme--text top-0">{memeData.topText}</h2>
+      <h2 className="meme--text bottom-0">{memeData.bottomText}</h2>
     </div>
   )
 }
