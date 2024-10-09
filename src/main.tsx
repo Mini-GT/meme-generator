64de183898d692929b6f756dedd15da22246a4e7
MiniGT <bendanillo539@gmail.com>
2024-10-09 23:38:00 +0800

    change to the correct reference

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+// import App from './App.tsx'
 // import App from './challenges/outputRandomURL/App'
 // import App from './challenges/addThingToTheArray/App'
 // import App from './challenges/statePractice/App'
@@ -14,11 +14,13 @@ import App from './App.tsx'
 // import Radio from './challenges/formInput/radio/Radio'
 // import Select from './challenges/formInput/select and input/Select'
 // import App from './challenges/formInput/final challenge/App'
+import PageUseEffect from './challenges/useEffect/Page'
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    {/* <App darkMode={false} /> */}
-    <App />
-  </StrictMode>,
+  // <StrictMode>
+  //   {/* <App darkMode={false} /> */}
+  //   <App />
+  // </StrictMode>,
+  <PageUseEffect /> // not using strictmode cause it renders the component twice(not using this time for a reason)
 )
