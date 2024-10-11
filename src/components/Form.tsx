1672c082cd1b30ec185201a47f4b339a4b8b8141
MiniGT <bendanillo539@gmail.com>
2024-10-11 21:37:27 +0800

    using async in useEffect explanation

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -12,6 +12,17 @@ export default function Form() {
     randomImage: "",
   })
 
+  //dont use async in useEffect. example `useEffect(async () => { })`
+  /**
+    useEffect takes a function as its parameter. If that function
+    returns something, it needs to be a cleanup function. Otherwise,
+    it should return nothing. If we make it an async function, it
+    automatically retuns a promise instead of a function or nothing.
+    Therefore, if you want to use async operations inside of useEffect,
+    you need to define the function separately inside of the callback
+    function, as seen below:
+    */
+  //for more explanation (https://v2.scrimba.com/learn-react-c0e/~045)
   useEffect(() => {
     const fetchData = async (url: string) => {
       try {
